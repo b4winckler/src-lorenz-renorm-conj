fc5e5c8854c05c1d10b65f51f68c42baf3172793
Bjorn Winckler <bjorn.winckler@gmail.com>
2018-04-25 10:08:37 +0100

    Output renormalizations in columns

--- renorm.cpp
+++ renorm.cpp
@@ -3,11 +3,19 @@
 
 #include "lorenz.h"
 
+static char usage[] =
+"usage: renorm w0 w1 nrenorm ngrid prec\n"
+"\n"
+"A Lorenz map as output by 'fixedpt' is read on stdin (can be piped).\n"
+"Evaluates 'nrenorm' (w0,w1)-renormalizations on 'ngrid' points for each\n"
+"branch, using 'prec' bits of precision.\n"
+"Outputs pairs of columns with (x, y) data for each renormalization.\n";
+
 
 int main(int argc, char *argv[])
 {
     if (argc != 6) {
-        std::cerr << "usage: renorm w0 w1 nrenorm ngrid prec\n\n";
+        std::cerr << usage << std::endl;
         exit(EXIT_FAILURE);
     }
 
@@ -16,11 +24,6 @@ int main(int argc, char *argv[])
     int nrenorm = atoi(argv[3]);
     int ngrid = atoi(argv[4]);
     int precision = atoi(argv[5]);
-    mpreal eps = machine_epsilon(precision);
-    std::cerr << "eps = " << eps << std::endl;
-    std::cerr << "combinatorics = " << w0 << '\t' << w1 << std::endl;
-    std::cerr << "nrenorm = " << nrenorm << ", ngrid = " << ngrid <<
-        ", prec = " << precision << std::endl;
 
     mpreal::set_default_prec(precision);
 
@@ -34,30 +37,43 @@ int main(int argc, char *argv[])
 
     renorm_op<mpreal> renorm(ctx, w0, w1);
 
-    vec<mpreal> x0(ngrid), y0(ngrid);
-    vec<mpreal> x1(ngrid), y1(ngrid);
+    vec<mpreal> x(2 * ngrid), y(2 * ngrid);
+    std::vector< vec<mpreal> > xs, ys;
 
-    std::cerr << "ctx = " << ctx.transpose() << std::endl;
-    for (int i = 0; i <= nrenorm; ++i, f = rf) {
-        std::cerr << "f" << i << "  = " << f.transpose() << std::endl;
-
-        x0.setLinSpaced(ngrid, 0, f[0]);
-        x1.setLinSpaced(ngrid, f[0], 1);
-
+    for (int i = 0; i <= nrenorm; ++i) {
+        // Evaluate f at evenly spaced points in interior of branches
+        x.head(ngrid) = vec<mpreal>::LinSpaced(ngrid, 0, f[0]);
+        x.tail(ngrid) = vec<mpreal>::LinSpaced(ngrid, f[0], 1);
         for (size_t j = 1; j < ngrid - 1; ++j) {
-            apply(y0[j], x0[j], f, ctx);
-            apply(y1[j], x1[j], f, ctx);
+            apply(y[j], x[j], f, ctx);
+            apply(y[ngrid + j], x[ngrid + j], f, ctx);
         }
 
-        y0[0] = f[1];
-        y0[ngrid - 1] = 1;
-        y1[0] = 0;
-        y1[ngrid - 1] = f[2];
+        // Evaluate f at boundary of branches
+        y[0] = f[1]; y[ngrid - 1] = 1;
+        y[ngrid] = 0; y[2 * ngrid - 1] = f[2];
 
-        print_vec(x0); std::cout << '\t'; print_vec(x1); std::cout << std::endl;
-        print_vec(y0); std::cout << '\t'; print_vec(y1); std::cout << std::endl;
+        xs.push_back(x); ys.push_back(y);
 
-        renorm(f, &rf);
+        if (i < nrenorm) {
+            renorm(f, &rf);
+            f = rf;
+        }
+    }
+
+    // Output header
+    std::cout << "x0\ty0";
+    for (int i = 1; i <= nrenorm; ++i)
+        std::cout << '\t' << "x" << i << '\t' << "y" << i;
+    std::cout << std::endl;
+
+    // Output columns of (x,y) data
+    // NB. No need to print with high precision since this is for plotting
+    for (size_t j = 0; j < 2 * ngrid; ++j) {
+        std::cout << xs[0][j] << '\t' << ys[0][j];
+        for (int i = 1; i <= nrenorm; ++i)
+            std::cout << '\t' << xs[i][j] << '\t' << ys[i][j];
+        std::cout << std::endl;
     }
 
     return EXIT_SUCCESS;
